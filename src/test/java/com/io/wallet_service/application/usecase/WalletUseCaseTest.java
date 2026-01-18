20846d993dab5a6766a62136f8d745cbdd8a7db1
Virgilius Nana Manek <virgilius.nana.manek0719@gmail.com>
2026-01-18 13:44:37 +0700

    chore: add new test service for idempotency keys

---
+++ src/test/java/com/io/wallet_service/application/usecase/WalletUseCaseTest.java
@@ -0,0 +1,24 @@
+package com.io.wallet_service.application.usecase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletUseCaseTest {
+
+    @Test
+    void credit() {
+    }
+
+    @Test
+    void debit() {
+    }
+
+    @Test
+    void transfer() {
+    }
+
+    @Test
+    void topUp() {
+    }
+}
